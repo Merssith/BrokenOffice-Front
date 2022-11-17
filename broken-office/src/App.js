175c17a0eed2b2427fc2cd67fc203b3f7475a7cd
tango72 <juanmaariasrodriguez@gmail.com>
2022-11-17 20:47:28 +0100

    ticketHistory

--- broken-office/src/App.js
+++ broken-office/src/App.js
@@ -2,21 +2,17 @@ import { Box } from "@mui/material";
 import { Route, Routes } from "react-router";
 import BottomNav from "./components/BottomNav";
 import Footer from "./components/Footer";
-import NavBar from "./components/NavBar";
 import HomeAdmin from "./containers/HomeAdmin";
 import NavBar from "./components/NavBar";
 import Home from "./components/Home";
 import Login from "./components/Login";
 import Register from "./components/Register";
+import UserTicketHistory from "./containers/UserTicketHistory";
 
 function App() {
   return (
     <Box display="flex" flexDirection="column">
       <NavBar />
-
-      <footer>
-        <BottomNav />
-      </footer>
 
       <Routes>
         {/* Public routes */}
@@ -30,12 +26,16 @@ function App() {
         <Route path="/user/profile" element={<></>} />
         <Route path="/user/profile/edit" element={<></>} />
         <Route path="/ticket/create" element={<></>} />
-        <Route path="/ticket/history" element={<></>} />
+        <Route path="/ticket/history" element={<UserTicketHistory/>} />
         <Route path="/ticket/:id" element={<></>} />
 
         {/* Admin routes*/}
         <Route path="/admin/*" element={<HomeAdmin />} />
       </Routes>
+
+      <footer>
+        <BottomNav />
+      </footer>
       {/* <Footer /> */}
     </Box>
   );
